7022e1cbd9531c8058d28d63a9ec549d7b4cfb1e
Miguel Angel Lopez <86500743+MiguelLopezMasmela@users.noreply.github.com>
2023-01-30 08:55:28 -0500

    UPDATE SCRPT SQL

--- script.sql
+++ script.sql
@@ -12,18 +12,18 @@ CREATE TABLE IF NOT EXISTS softwarePos_db.clientes (
 
 CREATE TABLE IF NOT EXISTS softwarePos_db.ventas (
   id_ventas INT NOT NULL,
-  cliente VARCHAR(45) NOT NULL,
-  total VARCHAR(45) NOT NULL,
-  pedido VARCHAR(45) NOT NULL,
+  cliente VARCHAR NOT NULL,
+  total INT NOT NULL,
+  pedido VARCHAR NOT NULL,
   PRIMARY KEY (id_ventas));
 
 
 
 CREATE TABLE IF NOT EXISTS softwarePos_db.pendientes (
   id_ventas INT NOT NULL,
-  cliente VARCHAR(45) NOT NULL,
-  total VARCHAR(45) NOT NULL,
-  pedido VARCHAR(45) NOT NULL,
+  cliente VARCHAR NOT NULL,
+  total INT NOT NULL,
+  pedido VARCHAR NOT NULL,
   domiciliario VARCHAR(45) NULL,
   PRIMARY KEY (id_ventas));
 
